0aae8109bb42708db76d082a90d80842d04e28e5
Parazoid <farismoh01@gmail.com>
2019-12-07 16:41:59 +0100

    added boilerplate and basic outline classes

--- sketches/windmill/windmill.js
+++ sketches/windmill/windmill.js
@@ -1,7 +1,7 @@
 let bgcolor; // #23272a
 let canvas;
 
-function setup() {
+function setup() { // Initial Setup
     canvas = createCanvas(windowWidth, windowHeight);
     bgcolor = color(35, 39, 42);
 }
@@ -10,6 +10,40 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
+function draw() { // Animation
     background(bgcolor);
+}
+
+class Dot {  
+    constructor() {
+        this.x;
+        this.y; 
+        this.r; // Radius
+        this.color;
+    }
+    
+    show() {
+
+    }
+
+
+}
+
+class Mill {
+    constructor() {
+        this.x;
+        this.y
+        this.len;
+        this.color;
+
+    }
+
+    show() {
+
+    }
+
+    pivot() {
+
+    }
+    
 }
