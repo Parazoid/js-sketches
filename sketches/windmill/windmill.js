70f75a3662f23281b6899f5bafaa81241a62746b
Parazoid <farismoh01@gmail.com>
2019-12-08 01:18:18 +0100

    added basic point functionality

--- sketches/windmill/windmill.js
+++ sketches/windmill/windmill.js
@@ -1,9 +1,14 @@
 let bgcolor; // #23272a
 let canvas;
+let points = [];
+let pointnum = 30;
 
 function setup() { // Initial Setup
     canvas = createCanvas(windowWidth, windowHeight);
     bgcolor = color(35, 39, 42);
+    for (let i = 0; i < pointnum; i++) {
+        points[i] = new Point(random(width-30), random(height-30), 5);
+    }
 }
 
 function windowResized() {
@@ -12,18 +17,23 @@ function windowResized() {
 
 function draw() { // Animation
     background(bgcolor);
+    for (let i = 0; i < pointnum; i++) {
+        points[i].show();
+    }
 }
 
-class Dot {  
-    constructor() {
-        this.x;
-        this.y; 
-        this.r; // Radius
+class Point {  
+    constructor(_x, _y, _r) {
+        this.x = _x;
+        this.y = _y; 
+        this.r = _r; // Radius
         this.color;
     }
     
     show() {
-
+        ellipse(this.x, this.y, this.r)
+        noStroke()
+        fill(255)
     }
 
 
@@ -45,5 +55,5 @@ class Mill {
     pivot() {
 
     }
-    
+
 }
