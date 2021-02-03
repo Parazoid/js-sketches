5248e8d845fcde99fa04b74abc38aef389ea5059
Parazoid <farismoh01@gmail.com>
2021-02-03 20:21:45 +0200

    removed some unnecessary pages stuff

---
+++ sketches/snowfall/snow.js
@@ -0,0 +1,15 @@
+let bgcolor; // #23272a
+let canvas;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    bgcolor = color(35, 39, 42);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(bgcolor);
+}
